df9ab2d53e094a9732460799831d56b08450b870
nick <nick.vanderwoude@donefirst.com>
2024-04-19 19:33:33 -0500

    fix: larger homepage fonts

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -4,6 +4,8 @@ import parseMarkdown from "../../utility/parseMarkdown";
 
 import { SectionProps } from "../../types/types";
 
+const bodyClass = 'font-body-lg text-light';
+
 const Section: React.FC<SectionProps> = ({
   heading,
   content,
@@ -16,9 +18,9 @@ const Section: React.FC<SectionProps> = ({
       {heading && <h2 className="font-heading-xl margin-y-0">{heading}</h2>}
       {content &&
         (typeof content === "string" ? (
-          <p>{parseMarkdown(content)}</p>
+          <p className={bodyClass}>{parseMarkdown(content)}</p>
         ) : (
-          content.map((item, index) => <p key={index}>{parseMarkdown(item)}</p>)
+          content.map((item, index) => <p className={bodyClass} key={index}>{parseMarkdown(item)}</p>)
         ))}
       {actionLabel && actionLink && (
         <a className="usa-button usa-button--big margin-top-3" href={actionLink}>
